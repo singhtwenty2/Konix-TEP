5ee62862c33f1e278ff2833cbcbf15e8d7c28075
Amaresh <amaresh9973kumar@gmail.com>
2024-07-23 10:34:55 +0530

    Added auto SQL generation

--- SERVER/src/main/kotlin/com/konix/configs/Databases.kt
+++ SERVER/src/main/kotlin/com/konix/configs/Databases.kt
@@ -1,7 +1,10 @@
 package com.konix.configs
 
+import com.konix.data.repository.entity.*
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
     Database.connect(
@@ -10,4 +13,20 @@ fun Application.configureDatabases() {
         user = System.getenv("USER"),
         password = System.getenv("PASSWORD")
     )
+    transaction {
+
+        SchemaUtils.create(
+            Users,
+            KYC,
+            DematAccounts,
+            Companies,
+            Exchanges,
+            CompanyExchanges,
+            StockPrices,
+            Orders,
+            Transactions,
+            Portfolios,
+            Otps
+        )
+    }
 }
