afce25c4992cf24ab68fa9a25def2fdb58a0d643
Aryan Singh <125189374+mrysg@users.noreply.github.com>
2024-08-07 20:07:07 +0530

    Minor Changes

--- SERVER/src/main/kotlin/com/konix/configs/Databases.kt
+++ SERVER/src/main/kotlin/com/konix/configs/Databases.kt
@@ -2,6 +2,8 @@ package com.konix.configs
 
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
     Database.connect(
